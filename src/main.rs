e86f4af4e97176b407fba6f426e3980ac2d587fd
Ivan Sim <ihcsim@gmail.com>
2025-12-17 12:35:20 -0800

    run rustfmt
    Signed-off-by: Ivan Sim <ihcsim@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use std::fs::{self,DirEntry};
+use std::fs::{self, DirEntry};
 use std::io;
 use std::path::Path;
 
-fn main() -> io::Result<()>{
-    let path = Path::new("./testdata/supportbundle_e4e6d62c-f3b9-4300-8426-1d8493b2b576_2025-10-27T18-38-27Z");
+fn main() -> io::Result<()> {
+    let path = Path::new(
+        "./testdata/supportbundle_e4e6d62c-f3b9-4300-8426-1d8493b2b576_2025-10-27T18-38-27Z",
+    );
     visit_dir(path, &search)?;
     Ok(())
 }
 
-fn visit_dir(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()>{
+fn visit_dir(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
         let path = entry.path();
