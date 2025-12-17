688be2e423de239b82007e962b24efb864e358e2
Ivan Sim <ihcsim@gmail.com>
2025-12-17 12:14:39 -0800

    add closure to search a file content
    Signed-off-by: Ivan Sim <ihcsim@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
-use std::fs;
+use std::fs::{self,DirEntry};
 use std::io;
 use std::path::Path;
 
-fn main() {
+fn main() -> io::Result<()>{
     let path = Path::new("./testdata/supportbundle_e4e6d62c-f3b9-4300-8426-1d8493b2b576_2025-10-27T18-38-27Z");
-    if path.is_dir() {
-        println!("this is a dir");
-        visit_dir(path);
-    }
+    visit_dir(path, &search)?;
+    Ok(())
 }
 
-fn visit_dir(dir: &Path) -> io::Result<()>{
+fn visit_dir(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()>{
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
         let path = entry.path();
         if path.is_dir() {
-            visit_dir(&path);
+            visit_dir(&path, cb)?;
         } else {
-            println!("found file: {}", entry.path().display());
+            search(&entry)
         }
     }
     Ok(())
+}
+
+fn search(entry: &DirEntry) {
+    println!(">> {:?}", entry.path())
 }
