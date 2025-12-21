f59bc260a82eafcc31d1e51daf643cd3c0c08d09
Ivan Sim <ihcsim@gmail.com>
2025-12-20 16:42:30 -0800

    sort entries in vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{self, DateTime, Utc};
 use regex::Regex;
 use std::fmt;
 use std::fs::{self, File};
@@ -9,21 +10,30 @@ const ROOT_DIR: &str =
 
 fn main() -> io::Result<()> {
     let path = Path::new(ROOT_DIR);
-    visit_dir(path, &search)?;
+    let mut entries: Vec<Entry> = Vec::new();
+    visit_dir(path, &mut entries, &search)?;
+    entries.sort_by(|a, b| a.timestamp.cmp(&b.timestamp));
+    for entry in &entries {
+        println!("{}", entry);
+    }
     Ok(())
 }
 
-fn visit_dir(dir: &Path, search: &dyn Fn(&Path, &str)) -> io::Result<()> {
+fn visit_dir(
+    dir: &Path,
+    v: &mut Vec<Entry>,
+    search: &dyn Fn(&Path, &mut Vec<Entry>, &str),
+) -> io::Result<()> {
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
         let path = entry.path();
         if path.is_dir() {
-            visit_dir(&path, search)?;
+            visit_dir(&path, v, search)?;
             continue;
         }
 
         if path.is_file() {
-            search(&path, "pvc-00b250c3-3e44-4cc8-a9c8-532621b4b1ea");
+            search(&path, v, "pvc-00b250c3-3e44-4cc8-a9c8-532621b4b1ea");
             continue;
         }
 
@@ -32,10 +42,11 @@ fn visit_dir(dir: &Path, search: &dyn Fn(&Path, &str)) -> io::Result<()> {
     Ok(())
 }
 
-fn search(path: &Path, s: &str) {
+fn search(path: &Path, v: &mut Vec<Entry>, s: &str) {
     let root = Path::new(ROOT_DIR);
     let regex_dt = Regex::new(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?Z").unwrap();
     let regex_lv = Regex::new(r"level=([^\s]+)").unwrap();
+
     if let Ok(file) = File::open(path) {
         let reader = io::BufReader::new(file);
         for line in reader.lines() {
@@ -45,6 +56,7 @@ fn search(path: &Path, s: &str) {
                 let subpath = path.strip_prefix(root).unwrap();
                 let identifier: Vec<&str> = subpath.to_str().unwrap().split('/').collect();
                 let timestamp = regex_dt.find(content.as_str()).unwrap();
+                let timestamp_fixed_offset = DateTime::parse_from_rfc3339(timestamp.as_str()).unwrap();
                 let level = match regex_lv.find(content.as_str()) {
                     None => "unknown",
                     Some(r) => r.as_str(),
@@ -55,13 +67,13 @@ fn search(path: &Path, s: &str) {
                         name: String::from(identifier[1]),
                         namespace: String::from(identifier[0]),
                     },
-                    content: &content,
+                    content: content.clone(),
                     level: String::from(level),
                     path: String::from(subpath.to_str().unwrap()),
-                    timestamp: String::from(timestamp.as_str()),
+                    timestamp: timestamp_fixed_offset.with_timezone(&Utc),
                 };
 
-                println!("{}", entry);
+                v.push(entry);
             }
         }
     } else {
@@ -70,15 +82,15 @@ fn search(path: &Path, s: &str) {
 }
 
 #[derive(Debug)]
-struct Entry<'a> {
+struct Entry {
     component: Component,
-    content: &'a String,
+    content: String,
     level: String,
     path: String,
-    timestamp: String,
+    timestamp: DateTime<Utc>,
 }
 
-impl<'a> fmt::Display for Entry<'a> {
+impl fmt::Display for Entry {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.content)
     }
