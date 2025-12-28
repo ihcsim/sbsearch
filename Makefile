e964606f4d0deb199661fbf273f059df7645fe3c
Ivan Sim <ihcsim@gmail.com>
2025-12-28 12:21:09 -0800

    update cmd arguments

--- Makefile
+++ Makefile
@@ -1,8 +1,19 @@
-run:
-	cargo run
-
 check:
 	cargo check
 
+run:
+	if [ -z "$(SUPPORT_BUNDLE_PATH)" ]; then \
+		echo "missing required argument: SUPPORT_BUNDLE_PATH"; \
+		exit 1; \
+	fi
+	if [ -z "$(RESOURCE_NAME)" ]; then \
+		echo "missing required argument: RESOURCE_NAME"; \
+		exit 1; \
+	fi
+	cargo run -- -s "$(SUPPORT_BUNDLE_PATH)" -r "$(RESOURCE_NAME)"
+
 release:
 	cargo build --release
+
+test:
+	cargo test
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ mod tui;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let key = args.key.as_str();
-    let root_dir = args.root_dir;
+    let resource_name = args.resource_name.as_str();
+    let root_dir = args.support_bundle_path + "/logs";
     let root_path = Path::new(root_dir.as_str());
 
-    let entries = sbfind::search(root_path, key)?;
+    let entries = sbfind::search(root_path, resource_name)?;
 
     let mut terminal = ratatui::init();
     tui::new(root_dir, entries).run(&mut terminal)?;
@@ -23,8 +23,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    root_dir: String,
+    support_bundle_path: String,
 
     #[arg(short, long)]
-    key: String,
+    resource_name: String,
 }
